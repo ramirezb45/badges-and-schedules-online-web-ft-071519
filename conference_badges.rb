92ac7ff668f12ef2a9fe186a8b62325fed0793c0
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-22 05:50:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+batch_badge_creater = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+brothers = ["Tim", "Tom", "Jim"]
+counter = 1
+brothers.each do |brother|
+  puts "This is loop number #{counter}"
+  puts "Stop hitting yourself #{brother}!"
+  counter += 1
+end
