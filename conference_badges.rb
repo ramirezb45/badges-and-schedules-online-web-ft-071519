1b14e205418813139f1ff2e9791f35fcfabb345f
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-22 05:55:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
+
 def batch_badge_creator(name)
   ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-end
 
 def assign_rooms(num)
   [1,2,3,4,5,6,7]
@@ -8,6 +8,8 @@ end
 
 puts "Hello #{name}! You'll be assigned to room #{num}!"
 
+end 
+  end 
 
 
 
@@ -20,10 +22,3 @@ puts "Hello #{name}! You'll be assigned to room #{num}!"
 
 
 
-brothers = ["Tim", "Tom", "Jim"]
-counter = 1
-brothers.each do |brother|
-  puts "This is loop number #{counter}"
-  puts "Stop hitting yourself #{brother}!"
-  counter += 1
-end
