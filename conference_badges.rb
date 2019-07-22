b8d9dade61d4a6bdfe42b315e7b1ab45d5de400f
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-22 05:56:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+
+def batch_badge_creator(name)
+  ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+def assign_rooms(num)
+  [1,2,3,4,5,6,7]
+end
+
+puts "Hello #{name}! You'll be assigned to room #{num}!"
+
+end 
+  end 
+
+
+
+
+
+
+
+
+
+
+
+
